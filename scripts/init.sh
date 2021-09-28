0a0bb10938ad0499aabf189847f12cca0618ee87
JunichiroKohari <kohari@sai-net.jp>
2021-09-28 20:41:35 +0900

    clone repos

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,8 +7,21 @@ cd ../
 ROOT=$(pwd)
 
 repos=(
+    'sp-api-service git@github.com:sai-edu/sp-api-service.git'
     'sp-view-service git@github.com:sai-edu/sp-view-service.git'
 )
 
 cd workspace
 
+for row in "${repos[@]}"; do
+    repo=(${row[@]})
+    dir=${repo[0]}
+    url=${repo[1]}
+    if [ -e ${dir} ]; then
+        echo "skip ${dir}"
+        continue
+    fi
+
+    echo "clone ${url}"
+    git clone ${url} ${dir}
+done
